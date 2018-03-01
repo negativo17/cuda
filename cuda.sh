cd2bda142241dfeaf0619681cfeb701d4f305d49
Simone Caronni <negativo17@gmail.com>
2018-03-01 18:57:29 +0100

    Re-add nvcc.profile

--- cuda.sh
+++ cuda.sh
@@ -1,3 +1,7 @@
-[ -x /usr/bin/nvcc ] && export NVVMIR_LIBRARY_DIR=/usr/share/cuda
-[ -x /usr/libexec/cuda/open64/bin/nvopencc ] && export PATH=$PATH:/usr/libexec/cuda/open64/bin
-[ -d /usr/include/cuda ] && export CUDA_INC_PATH=/usr/include/cuda
+if [ -x /usr/bin/cuda-g++ ]; then
+  export HOST_COMPILER=/usr/bin/cuda-g++
+fi
+
+if [ -x /usr/libexec/cuda/open64/bin/nvopencc ]
+  export PATH=$PATH:/usr/libexec/cuda/open64/bin
+fi
