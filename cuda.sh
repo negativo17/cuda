e95b7006a44c987854fc683bdccb7940a5c428e6
Simone Caronni <negativo17@gmail.com>
2018-03-02 16:26:53 +0100

    Add CUDA_INCLUDE_DIRS to profile.d

--- cuda.sh
+++ cuda.sh
@@ -2,6 +2,10 @@ if [ -x /usr/bin/cuda-g++ ]; then
   export HOST_COMPILER=/usr/bin/cuda-g++
 fi
 
-if [ -x /usr/libexec/cuda/open64/bin/nvopencc ]
+if [ -x /usr/libexec/cuda/open64/bin/nvopencc ]; then
   export PATH=$PATH:/usr/libexec/cuda/open64/bin
+fi
+
+if [ -d /usr/include/cuda ]; then
+  export CUDA_INCLUDE_DIRS=/usr/include/cuda
 fi
