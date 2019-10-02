a1a527752e4f97d746c295b69e7051a0d1ea7167
Simone Caronni <negativo17@gmail.com>
2019-10-02 11:16:22 +0200

    Update to 10.1 Update 2

--- cuda-generate-tarball.sh
+++ cuda-generate-tarball.sh
@@ -3,7 +3,7 @@ set -e
 
 PKG=cuda
 MAJOR_VERSION=${MAJOR_VERSION:-10.1}
-VERSION=${VERSION:-10.1.168}
+VERSION=${VERSION:-10.1.243}
 TARBALL=${PKG}-${VERSION}-x86_64
 
 get_run_file() {
@@ -13,14 +13,18 @@ get_run_file() {
 }
 
 # Main installer
-DL_SITE=https://developer.nvidia.com/compute/cuda/$MAJOR_VERSION/Prod/local_installers
-RUN_FILE=cuda_${VERSION}_418.67_linux.run
+DL_SITE=http://developer.download.nvidia.com/compute/cuda/$MAJOR_VERSION/Prod/local_installers
+RUN_FILE=cuda_${VERSION}_418.87.00_linux.run
 get_run_file
 
 # Unpack installer
-sh $RUN_FILE --extract=`pwd` --override
+rm -fr extract
+mkdir -p extract
+sh $RUN_FILE --extract=$(pwd)/extract --override --override-driver-check --silent
 
 printf "Creating tarball ${TARBALL}... "
+
+cd extract
 
 # Remove binaries included in system packages
 rm -fr cuda-toolkit/jre
@@ -35,11 +39,19 @@ rm -fr cuda-toolkit/bin/cuda-uninstaller \
     cuda-samples/bin
 
 # Move out gdb sources
-mv cuda-toolkit/extras/${PKG}-gdb-*.src.tar.gz .
+mv cuda-toolkit/extras/${PKG}-gdb-*.src.tar.gz ..
+
+# Remove extra architectures for tools
+rm -fr \
+    cuda-toolkit/nsight-compute-*/target/linux-desktop-glibc*x86 \
+    cuda-toolkit/nsight-compute-*/target/linux-desktop-glibc*ppc64le
 
 # Create tarball
-mkdir ${TARBALL}
-mv cuda-toolkit cuda-samples ${TARBALL}
+mkdir ../${TARBALL}
+mv cuda-toolkit cuda-samples ../${TARBALL}
+cd ..
 tar --remove-files -cJf ${TARBALL}.tar.xz ${TARBALL}
+
+rm -fr extract
 
 printf "OK\n"
